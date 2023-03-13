2a1fb97b40c1cd63b948b76a25fb2bbd889f6663
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-03-13 18:12:26 +0545

    added side list on music player

--- js basic/mp3Player/index.js
+++ js basic/mp3Player/index.js
@@ -6,29 +6,31 @@ const rightBtn = document.querySelector("#right");
 const playBtn = document.querySelector(".playBtn");
 const pauseBtn = document.querySelector(".pauseBtn");
 const audioHtmlName = document.querySelector("#audioHtmlName");
+const audioNameList = document.querySelector("#audioNameList");
 
 var playing = false;
 var audioNum = 2;
 var audioPlaying = document.getElementById("myAudio");
-audioPlaying.loop = true
+audioPlaying.loop = true;
 
 function changedAudio() {
   thumbnailImg.src = `audios/${audioName[audioNum]}.jpg`;
   audioPlaying.src = `audios/${audioName[audioNum]}.mp3`;
-  audioHtmlName.innerHTML = audioName[audioNum]
-  if(playing){
-    audioPlaying.play()
+  audioHtmlName.innerHTML = audioName[audioNum];
+  if (playing) {
+    audioPlaying.play();
   }
+  updateList();
 }
 
 play.addEventListener("click", (e) => {
-    console.log("play")
+  console.log("play");
   if (playing) {
     audioPlaying.pause();
     playing = false;
     playBtn.style.display = "none";
     pauseBtn.style.display = "block";
-} else {
+  } else {
     audioPlaying.play();
     playing = true;
     pauseBtn.style.display = "none";
@@ -37,19 +39,35 @@ play.addEventListener("click", (e) => {
 });
 
 leftBtn.addEventListener("click", (e) => {
-    audioNum--;
-    if(audioNum < 0){
-        audioNum = audioName.length - 1;
-    }
-    changedAudio();
+  audioNum--;
+  if (audioNum < 0) {
+    audioNum = audioName.length - 1;
+  }
+  changedAudio();
 });
 
 rightBtn.addEventListener("click", (e) => {
-    audioNum++;
-    if(audioNum > audioName.length -1 ){
-        audioNum = 0;
-    }
-    changedAudio();
+  audioNum++;
+  if (audioNum > audioName.length - 1) {
+    audioNum = 0;
+  }
+  changedAudio();
 });
 
-changedAudio()
+for(let i=0; i<audioName.length; i++){
+  audioNameList.innerHTML += `<li onClick="">${audioName[i]}</li>`
+}
+ 
+function updateList(){
+  audioNameList.innerHTML = ""
+  for(let i=0; i<audioName.length; i++){
+    audioNameList.innerHTML += `<li style="${ i == audioNum?'background: #a000ff40': ''}" onClick="changeAudioPlaying(${i})">${audioName[i]}</li>`
+  }
+}
+
+function changeAudioPlaying(idx){
+  audioNum = idx;
+  changedAudio();
+}
+
+changedAudio();
