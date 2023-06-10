13e90d6f9f09dab0bcef5ba29d68a78ef69f3c1b
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-06-10 08:10:07 +0545

    jQuery example implemented

---
+++ 4th sem codes/jQuery/js/main.js
@@ -0,0 +1,54 @@
+// $(p).hide()
+
+// $('p').dblclick(function (){
+//     $(this).hide();
+// })
+
+// $('p.para').click();
+
+// $('p.para').hide()
+
+
+$(document).ready(function(){
+
+    // $("input").focus(function(){
+    //   $(this).css("background-color", "yellow");
+    // });
+
+    // $("input").blur(function(){
+    //   $(this).css("background-color", "green");
+    // });
+
+    // $("p").on("click", function(){
+    //     $(this).hide();
+    // });
+
+    // $("p").on({
+    //     mouseenter: function(){
+    //       $(this).css("background-color", "lightgray");
+    //     },
+    //     mouseleave: function(){
+    //       $(this).css("background-color", "lightblue");
+    //     },
+    //     click: function(){
+    //       $(this).css("background-color", "yellow");
+    //     }
+    // });
+
+
+    // $('p').hide(2000)
+    // $('p').show(1000)
+    // $('p').toggle(3000)
+
+    // $('p').slideUp(1000)
+    // $('p').slideDown(1000)
+    // $('p').slideToggle('slow')
+
+    // $('#head').animate({width: "40%"}, 3000)
+
+    // $("#para")
+	// .css("color", "red")
+	// .slideUp(2000)
+	// .slideDown(2000);
+
+  });
